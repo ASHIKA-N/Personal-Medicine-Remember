deb1fb8c76aa1ef7d8d7e44de3b258535f61e11f
WingedKing2311 <marcusabrahamt@gmail.com>
2025-09-25 20:37:01 +0530

    Update DS.cpp

--- DS.cpp
+++ DS.cpp
@@ -3,10 +3,13 @@ using namespace std;
 #include <cstdlib>
 
 class Stack{
-  int *a;
-  int size;
+  int *a,size,top;
 void reallc(int i=1){
-  a=(int*)realloc(a,(size+i)*sizeof(int));
+  int *temp=new int[size+i];
+  for(int in=0;in<size;in++)
+    temp[i]=a[i];
+  delete[] a;
+  a=temp;
   size+=i;
 }
   public:
@@ -19,9 +22,8 @@ void reallc(int i=1){
     delete[] a;
   }
   void push(int v){
-    if(isFull()){
-      reallc();
-    }
+    if(isFull())
+      reallc(size);
     a[++top]=v;
   }
   int pop(){
@@ -50,35 +52,59 @@ void reallc(int i=1){
 class Queue{
   int *a;
   int size;
-  int front,rear;
+  int front,rear,count;
   void reallc(int i=1){
-    a=(int*)realloc(a,(size+i)*sizeof(int));
+    int *temp=new int[size+i];
+    for(int in=0;in<size;in++)
+      temp[i]=a[i];
+    delete[] a;
+    a=temp;
     size+=i;
   }
   public:
   Queue(int s=5){
+    count=0;
     size=s;
     front=rear=-1;
-    a=new int[s];
+    a=new int[size];
   }
   ~Queue(){
     delete[] a;
   }
   void enqueue(int v){
-    if(front==(rear+1)%size){
-      reallc();
-      ;
-    if(front==-1){
-    ;}
-      bool isFull(){
-    return top==size-1;
+    if(isFull())
+      reallc(size);
+    a[++rear]=v;
+    if(isEmpty())
+      ++front;
+  }
+  void deque(){
+    if(isEmpty()){
+      cout<<"Queue Underflow";
+      return -1;
+    }
+    if(front==rear) rear=-1;
+    int v=a[front++];
+    if(rear==-1) front=-1;
+    return v;
+  }
+  int peek(){
+    if(isEmpty()){
+      cout<<"Stack Underflow";
+      return -1;
+    }
+    return a[front];
+  }
+      
+  bool isFull(){
+    return front==(rear+1)%size;
   }
 
   bool isEmpty(){
-    return top==-1;
+    return front==-1;
   }
     
-    };
+};
 
 
     
