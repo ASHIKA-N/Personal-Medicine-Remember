4052cfa1141f76dcf0f7a639316decc0b1d0d033
Ashika <nashika231@gmail.com>
2025-09-25 13:05:47 +0530

    Create DS.cpp

---
+++ DS.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+#include <cstdlib>
+
+class Stack{
+  int *a;
+  int size;
+void reallc(int i=1){
+  a=(int*)realloc(a,(size+i)*sizeof(int));
+  size+=i;
+}
+  public:
+  Stack(int s=5){
+    top=-1;
+    size=s;
+    a=new int[s];
+  }
+  void push(int v){
+    if(top==size-1){
+      reallc();
+    }
+    a[++top]=v;
+  }
+  int pop(){
+    if(top==-1){
+      cout<<"Stack Underflow";
+      return -1;
+    }
+    return a[top--];
+  }
+  int peek(){
+    if(top==-1){
+      cout<<"Stack Underflow";
+      return -1;
+    }
+    return a[top];
+};
+    
+    
+    
