ffc28b0bd11bedd5ded54bfdea0427708f8c11b4
WingedKing2311 <marcusabrahamt@gmail.com>
2025-09-28 22:19:02 +0530

    Update Struct.hpp

--- hppfolder/Struct.hpp
+++ hppfolder/Struct.hpp
@@ -1,23 +1,43 @@
+#pragma once
+#include <iostream>
 #include <string>
-#include "Struct.hpp"
 using namespace std;
 
 struct Time{
   int h,m;
+
+  Time(int hour = 0, int min = 0) {
+    if (hour >= 0 && hour < 24) h = hour;
+    else {
+      cout << "Invalid hour! Setting to 0.\n";
+      h = 0;
+    }
+    if (min >= 0 && min < 60) m = min;
+    else {
+      cout << "Invalid minute! Setting to 0.\n";
+      m = 0;
+    }
+  }
+
   int toMinutes() const {
     return h * 60 + m;
   }
 };
 
-struct med {
+struct Med {
   string name;
   string dosage;
   Time t;
-med(string& n,string& d, int hour, int min) {
+  int fpw;
+
+  Med(const string& n,const string& d, int hour, int min,int f) {
     name = n;
     dosage = d;
     t.h = hour;
     t.m = min;
+    if(f<1 || f>7)
+      f=7;
+    fpw=f;
   }
 };
   
