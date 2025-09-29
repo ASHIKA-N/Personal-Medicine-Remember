7f147a0fe0a6d4ff182a19c9034c1812c2896678
WingedKing2311 <marcusabrahamt@gmail.com>
2025-09-29 14:51:12 +0530

    Update main.cpp

--- cppfolder/main.cpp
+++ cppfolder/main.cpp
@@ -1,5 +1,38 @@
 #include <iostream>
+#include "../include/DS.hpp"
 using namespace std;
+
+void menu(){
+  cout<<"\n===Personal Medication Reminder===\n";
+  cout<<"Choices\n";
+  cout<<"1.View Med Schedule\n";
+  cout<<"2.Insert New Med\n";
+  cout<<"3.Delete a Med\n";
+  cout<<"4.Alter a Med\n";
+  cout<<"5.Search for a Med\n";
+  cout<<"6.Exit\n";
+}
+
 int main(){
-  cout<<"Yo";
+  int ch;
+  while(1){
+    menu();
+    cin<<ch;
+    switch(ch){
+      case 1:
+      break;
+      case 2:
+      break;
+      case 3:
+      break;
+      case 4:
+      break;
+      case 5:
+      break;
+      case 6:
+      return 0;
+      default:
+      cout<<"Invalid Choice, Try Again";
+    }
+  }
 }
