0f1c1c967a3dc264f4171ae44b046ff29a538bc1
ashika <nashika231@gmail.com>
2025-10-01 18:03:46 +0530

    Altered medicine

--- cppfolder/main.cpp
+++ cppfolder/main.cpp
@@ -17,8 +17,10 @@ void menu(){
 }
 void altermed(){
 cout<<"Choices\n";
-cout<<"1.Change medicine\n";
-cout<<"2.Change medicine time\n";
+cout<<"1.Alter medicine\n";
+cout<<"2.Alter medicine time\n";
+cout<<"3.Alter medicine and time\n";
+
 }
 int main(){
   int ch,ch1;
@@ -56,20 +58,39 @@ int main(){
         case 1:
         string old_med,new_med;
         cout<<"Enter old medicine name : ";
-        cin>>old_med;
+        getline(cin,old_med);
         cout<<"Enter new medicine name: ";
-        cin>>new_med;
+        getline(cin,new_med);
         string res=rename(old_med.c_str(),new_med.c_str());
           break;
         case 2:
-          string med;
+          string med_name,date_time;
           time_t upd_time;
+          struct tm tmStruct={};
           cout<<"Enter medicine name: \n";
           cin>>med_name;
           cout<<"Enter medicine time: \n";
-          cin>>upd_time;
+          getline(cin,date_ime);
+          strptime(date_time.c_str(),"%Y-%m-%d %H:%M:%S",&tmStruct);
+          upd_time=mktime(&tmStruct);
           break;
-        }
+          case 3:
+          string ol_med,ne_med;
+        cout<<"Enter old medicine name : ";
+        getline(cin,ol_med);
+        cout<<"Enter new medicine name: ";
+        getline(cin,ne_med);
+        string resu=rename(ol_med.c_str(),ne_med.c_str());
+        string dat_time;
+          time_t up_time;
+          struct tm tmStruct={};
+          cout<<"Enter medicine time: \n";
+          getline(cin,dat_ime);
+          strptime(dat_time.c_str(),"%Y-%m-%d %H:%M:%S",&tmStruct);
+          up_time=mktime(&tmStruct);
+          break;
+       
+}
       break;
       case 6:
       string ns;
