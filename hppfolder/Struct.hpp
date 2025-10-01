2dcba995684d4188500cd35be47ccc7377eee5fd
WingedKing2311 <marcusabrahamt@gmail.com>
2025-10-01 14:52:15 +0530

    Update Struct.hpp

--- hppfolder/Struct.hpp
+++ hppfolder/Struct.hpp
@@ -32,13 +32,49 @@ struct Time{
   }
 };
 
+struct Date{
+  int d,m,y;
+
+  Date(int day, int month, int year) {
+    if (isValid(day, month, year)) {
+      d = day;
+      m = month;
+      y = year;
+    } else {
+      cout << "Invalid date! Setting default 1/1/2000\n";
+      d = 1;
+      m = 1;
+      y = 2000;
+    }
+  }
+
+  static bool isValid(int day, int month, int year) {
+    if (year < 1) return false;
+    if (month < 1 || month > 12) return false;
+
+    int daysInMonth[] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+
+    bool leap = (year % 400 == 0) || (year % 100 != 0 && year % 4 == 0);
+    if (leap) daysInMonth[2] = 29;
+
+    if (day < 1 || day > daysInMonth[month]) return false;
+
+    return true;
+  }
+
+  void print() {
+    cout << d << "/" << m << "/" << y << "\n";
+  }
+};
+
 struct Med {
   string name;
   string dosage;
   Time t;
+  Date exp;
   int fpw;
 
-  Med(const string& n,const string& d, int hour, int min,int f) {
+  Med(const string& n,const string& d, int hour, int min,int f,int a,int b,int c) {
     name = n;
     dosage = d;
     t.h = hour;
@@ -46,6 +82,8 @@ struct Med {
     if(f<1 || f>7)
       f=7;
     fpw=f;
+    exp.d=a;
+    ex
   }
 
   void disp(){
